6828a63425239d5ec50779d965c16dcb1df4be6f
南门 <Yu.Song@cisdi.com.cn>
2018-11-13 22:36:39 +0800

    feat: 线程

--- DesignPatternDemo/MVC/NMMVCModel.m
+++ DesignPatternDemo/MVC/NMMVCModel.m
@@ -11,21 +11,23 @@
 @implementation NMMVCModel
 
 + (void)getMokeDataWithHandlerBlock:(void (^)(NSArray<NMMVCModel *> * _Nonnull, NSError * _Nonnull))handler {
-    NSMutableArray *mokeData = [NSMutableArray array];
-    NSError *error;
-    for (int i = 0; i < 10; ++i) {
-        NMMVCModel *model = [NMMVCModel new];
-        model.modelName = NSStringFromClass(self);
-        model.modelNumber = i;
-        [mokeData addObject:model];
-    }
-    if (mokeData.count == 0) {
-        error = [NSError errorWithDomain:@"No data!" code:-1 userInfo:nil];
-    }
-    
-    // 模拟网络请求，卡1秒
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        !handler?:handler(mokeData.copy, error);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSMutableArray *mokeData = [NSMutableArray array];
+        NSError *error;
+        for (int i = 0; i < 10; ++i) {
+            NMMVCModel *model = [NMMVCModel new];
+            model.modelName = NSStringFromClass(self);
+            model.modelNumber = i;
+            [mokeData addObject:model];
+        }
+        if (mokeData.count == 0) {
+            error = [NSError errorWithDomain:@"No data!" code:-1 userInfo:nil];
+        }
+        
+        // 模拟网络请求，卡1秒
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            !handler?:handler(mokeData.copy, error);
+        });
     });
 }
 
